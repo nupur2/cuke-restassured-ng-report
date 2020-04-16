bba871f2465880e8a4b277f330fee6a683111a12
nupur <nupuragrawal43@gmail.com>
2020-04-16 22:04:03 +0530

    added basic auth

--- src/main/java/common/constatnt/LinkConstant.java
+++ src/main/java/common/constatnt/LinkConstant.java
@@ -3,4 +3,6 @@ package common.constatnt;
 public class LinkConstant {
 
 	public static String ENDPOINT_GET_BOOK_BY_ISBN = "https://www.googleapis.com/books/v1/volumes";
+	
+	public static String baseUrl = "https://samples.openweathermap.org/data/2.5/";
 }
--- src/main/java/common/helper/CommonFunctions.java
+++ src/main/java/common/helper/CommonFunctions.java
@@ -3,22 +3,41 @@ package common.helper;
 import static io.restassured.RestAssured.given;
 
 import common.constatnt.LinkConstant;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
-
 
 public class CommonFunctions {
 	private Response response;
 	private ValidatableResponse json;
 
+	public RequestSpecification basicAuth() {
+		return given().auth().basic("user1", "user1Pass");
+	}
+
 	public Response getGetRequestBuilder(RequestSpecification request) {
 		return request.when().get(LinkConstant.ENDPOINT_GET_BOOK_BY_ISBN);
 	}
-	public RequestSpecification getParams(String key , String value) {
+
+	public RequestSpecification getParams(String key, String value) {
 		return given().param(key, value);
 	}
+
 	public ValidatableResponse getStatusCode(Response response, int statusCode) {
 		return response.then().statusCode(statusCode);
 	}
+
+	public RequestSpecification getBaseUrl() {
+		return RestAssured.given().baseUri(LinkConstant.baseUrl);
+	}
+
+	public RequestSpecification setParams(RequestSpecification request, String key, String value) {
+		return request.queryParam(key, value);
+	}
+
+	public Response getData(RequestSpecification request, String data) {
+		return request.get("/" + data);
+	}
+
 }
